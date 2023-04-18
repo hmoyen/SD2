67371652aa14816ade6c130b2c2e03caa6920719
helena <helenamoyen@usp.br>
2023-04-18 15:37:13 -0300

    new adder

--- Aula19.04/somador.v
+++ Aula19.04/somador.v
@@ -1,16 +1,13 @@
-module somador(
+module somador(a,b,sinal,soma);
 
-    input [PARAM_BITS-1:0] a,
-    input [PARAM_BITS-1:0] b,
-    input sinal,
-    output reg soma [PARAM_BITS:0]
-);
-
-    parameter PARAM_BITS = 8;
+    input [7:0] a;
+    input [7:0] b;
+    input sinal;
+    output reg [8:0] soma;
 
     always @(a,b) begin
         if (sinal == 1'b0) begin
-            soma <= a + b;
+            soma = a + b;
         end  
         else begin
             soma <= a - b;
