fbf415e30776797e73ea06227707b33ef96062a1
marimaritian <105123923+marimaritian@users.noreply.github.com>
2023-06-14 13:03:17 -0300

    metade das instruções

--- aula14junho/memoria.v
+++ aula14junho/memoria.v
@@ -92,13 +92,35 @@ initial begin
    // Depois do sign extend do imediato, fazemos um shift left 2, multiplicando por 4
    // Ou seja, indo do registrador[i] para o registrador [i+8], o que seria, justamente, duas instruções para frente
 
-    registradores[0] = {1'b0, 6'b000000, 5'b00001, 5'b00010, 3'b000, 4'b0010, 1'b0, 7'b1100011}; // BEQ, compara x1 e x2
-    registradores[2] = {12'b000000000011, 5'b00011, 3'b010, 5'b00111, 7'b0000011}; // LOAD, escreve no x7 o que está no x3 #3 da memoria (0 + 3 = 3), logo o número 14
-    registradores[3] = {7'b1111111, 5'b01000, 5'b00111, 3'b010, 5'b11111, 7'b0100011}; // STORE, armazena o valor do x8 no x7 #-1 da memoria
-    registradores[4] = {7'b0000000, 5'b01000, 5'b00001,3'b000,  5'b01001, 7'b0110011}; // ADD, armazena o valor x8 + x1 no x9
-    registradores[5] = {7'b0100000, 5'b00001, 5'b01000, 3'b000,  5'b01010, 7'b0110011}; // SUB, armazena o valor x8 - x1 no x10
-    registradores[6] = {7'b0000000, 5'b00001, 5'b00010, 3'b111, 5'b01100, 7'b0110011}; // AND entre x1 e x2, armazena valor no x12
-    registradores[7] = {7'b0000000, 5'b00001, 5'b00011, 3'b110, 5'b01101, 7'b0110011}; // OR entre x1 e x3, armazena valor no x13
+    registradores[0] = {1'b0, 6'b000000, 5'b00001, 5'b00010, 3'b000, 4'b1010, 1'b0, 7'b1100011}; // BEQ, compara x1 e x2. Se forem diferentes, vai para
+                                                                                                //a instrução dos registradores[1], se forem iguais, vai para registradores[8]
+
+    registradores[1] = {7'b0100000, 5'b00001, 5'b00010, 3'b000,  5'b01010, 7'b0110011}; // SUB, armazena o valor x1 - x2 no x10, e depois vai para a instrução registradores[3]
+    registradores[2] = {7'b0000000, 5'b01010, 5'b00110, 3'b111, 5'b01100, 7'b0110011}; // AND entre x10 e x6, armazena valor no x12.(No x6 está o 1000000000 
+                                                                                        //a fim de comparar com o valor do resultado do sub, e verificar se tal valor é negativo)
+
+    registradores[3] = {1'b0, 6'b000000, 5'b01100, 5'b00000, 3'b000, 4'b0010, 1'b0, 7'b1100011}; // BEQ, compara x12 e x0 que vale 0. Se o BEQ apontar que é igual, então o resultado de 
+                                                                                                //AND foi 0 e portanto, x2>x1. Nesse caso, ele vai pular para a instrução registradores[6]
+
+    registradores[4] = {7'b0100000, 5'b00010, 5'b00001, 3'b000,  5'b00001, 7'b0110011}; // SUB entre x1-x2, armazena em x1, pois x1>x2
+
+
+    //////////aki deu errado
+    
+    registradores[5] = {7'b0000000, 5'b01000, 5'b00001,3'b000,  5'b01001, 7'b0110011}; // ADD, armazena o valor x8 + x1 no x9
+    registradores[8] = {1'b0, 6'b000000, 5'b00001, 5'b00010, 3'b000, 4'b1010, 1'b0, 7'b1100011}; // Mesmo BEQ que compara, e se forem diferentes, então vai para registradores[1]
+
+    registradores[6] = {7'b0100000, 5'b00001, 5'b00010, 3'b000,  5'b00010, 7'b0110011}; // SUB, entre x2-x1 armazena o valor no x2
+    registradores[7] = {1'b0, 6'b000000, 5'b00001, 5'b00010, 3'b000, 4'b0001, 1'b0, 7'b1100011};// Mesmo BEQ do inicio, porem agora retorna para o inicio se forem diferentes
+
+
+
+
+
+    registradores[11] = {7'b0100000, 5'b00001, 5'b01000, 3'b000,  5'b01110, 7'b0110011}; // SUB, armazena o valor x8 - x1 no x10
+    registradores[7] = {7'b1111111, 5'b01000, 5'b00111, 3'b010, 5'b11111, 7'b0100011}; // STORE, armazena o valor do x8 no x7 #-1 da memoria
+    registradores[9] = {7'b0000000, 5'b00001, 5'b00010, 3'b111, 5'b01100, 7'b0110011}; // AND entre x1 e x2, armazena valor no x12
+    registradores[10] = {7'b0000000, 5'b00001, 5'b00010, 3'b111, 5'b01100, 7'b0110011}; // BEQ para acabar com a simulação
     
     
 end
